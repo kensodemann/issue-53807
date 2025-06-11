6131f9cea63bd7e50c0c12c17cfb23e664e460fe
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-06-11 08:05:20 -0500

    implement some inputs, etc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonInput, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonContent, IonInput, IonItem, IonLabel, IonList],
 })
 export class HomePage {
-  constructor() {}
+  getHelp() {
+    const days = Math.floor(Math.random() * 365) + 40;
+    const message = `We have heard your pleas for help. Help should arive in roughly ${days} days. May the force be with you in the meantime. `;
+    alert(message);
+  }
 }
